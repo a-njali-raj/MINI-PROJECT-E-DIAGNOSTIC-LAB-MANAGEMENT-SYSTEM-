d018219961a7ed5c4530dcd3526debea7fb1b5ef
Anjali Raj <117928198+a-njali-raj@users.noreply.github.com>
2023-11-02 06:52:50 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path
-from.import views
+from .import views
 
 
 urlpatterns = [
-    path('',views.index,name="home"),
-    path('index.html',views.index,name="home"),
-    path('about.html',views.about,name="about"),
-    path('services.html',views.services,name="services"),
-    path('contact.html',views.contact,name="contact"),
-    path('login.html',views.loginn,name="login"),
-    path('signup.html',views.signup,name="signup"),
-    path('appoinment.html',views.appoinment,name="appoinment"),
-    path('user.html',views.user,name="user"),
-    path('logout',views.logout,name="logout"),
-    path('services1.html',views.services1,name="services1"),
+    path('staff-dashboard',views.staff_dashboard, name="staff_dashboard"),
+    path('admin-dashboard',views.admin_dashboard, name="admin_dashboard"),
+    path('userdetails.html',views.userdetails, name="userdetails"),
+    path('admintest.html',views.admintest, name="admintest"),
+    path('addtest.html', views.addtest, name="addtest"),
+    path('updatetest.html', views.updatetest, name="updatetest"),
+    path('delete_test/<int:test_id>/', views.delete_test, name='delete_test'),
+    path('adminstaff.html', views.adminstaff, name="adminstaff"),
 ]
+
--- views.py
+++ views.py
@@ -1,48 +1,97 @@
-from django.shortcuts import render,redirect,HttpResponse
-from django.contrib.auth import authenticate,login
-from django.contrib.auth.models import User
-from django.contrib import messages
-from django.contrib import auth
+from django.shortcuts import render, redirect, HttpResponse,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from tests.models import User 
+from django.views.decorators.cache import never_cache
+from tests.models import Test
+@never_cache
+@login_required(login_url='login')
+def staff_dashboard(request):
+    if request.user.is_staff and not request.user.is_superuser:
+        
+        return render(request, "staff_dashboard.html")
+    return redirect("home")
 
-# Create your views here.
-def index(request):
-    return render(request,'index.html')
-def about(request):
-    return render(request,'about.html')
-def services(request):
-    return render(request,'services.html')
+@never_cache
+@login_required(login_url='login')
+def admin_dashboard(request):
+    if request.user.is_superuser:
+        users = User.objects.filter(is_superuser=False, is_staff=False)
+        return render(request, "admin_dashboard.html", {"users": users})
+    return redirect("home")
+@never_cache
+@login_required(login_url='login')
+def userdetails(request):
+    if request.user.is_superuser:
+        users = User.objects.filter(is_superuser=False, is_staff=False)
+        return render(request, "userdetails.html", {"users": users})
+    return redirect("home")
+   
+@never_cache
+@login_required(login_url='login')
+def admintest(request):
+    tests = Test.objects.all() 
+    return render(request, "admintest.html", {'tests': tests}) # Retrieve all Test objects from the database
+@never_cache
+@login_required(login_url='login')
+def addtest(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        is_available = request.POST.get('is_available')  # Get the checkbox value
 
-def contact(request):
-    return render(request,'contact.html')
-def loginn(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('user')
+        # If the checkbox is not checked, set is_available to False
+        if is_available is None:
+            is_available = False
         else:
-            messages.error(request, 'Invalid username or password')  # Add an error message
-            return redirect('login')  # Redirect back to the login page
+            is_available = True
 
-    return render(request, 'login.html')
-def signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        myuser = User.objects.create_user(username,email,password)
-        myuser.save()
-        return redirect('login')
-    return render(request,'signup.html')
+        if name and price:
+            Test.objects.create(name=name, price=price, is_available=is_available)
+            return redirect("admintest")  # Redirect to a success page or any other page
+    return render(request, "addtest.html")
 
-def appoinment(request):
-    return render(request,'appoinment.html')
-def user(request):
-    return render(request,'user.html')
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
-def services1(request):
-    return render(request,'services1.html')
+def delete_test(request,test_id):
+    test = get_object_or_404(Test, id=test_id)
+
+    # Instead of deleting, change the availability status to False
+    test.is_available = False
+    test.save()
+
+    return redirect("admintest")
+@never_cache
+@login_required(login_url='login')
+def updatetest(request):
+    # Retrieve 'test_id' from the query parameter 'test_id'
+    test_id = request.GET.get('test_id')
+
+    if request.method == 'POST':
+        # Get the updated data from the form
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        availability = request.POST.get('availability')
+
+        # Retrieve the test instance based on the 'test_id'
+        test = Test.objects.get(pk=test_id)
+
+        # Update the test's attributes
+        test.name = name
+        test.price = price
+        test.is_available = availability == "True"  # Convert to boolean
+
+        # Save the updated test
+        test.save()
+
+        # Redirect to a success page or return a response
+        return redirect('admintest')  # Replace 'success_page' with the appropriate URL
+
+    # Handle GET requests for editing the test details
+    else:
+        # Retrieve the test instance based on the 'test_id'
+        test = Test.objects.get(pk=test_id)
+        context = {
+            'test': test,
+        }
+        return render(request, "updatetest.html", context)
+@never_cache
+def adminstaff(request):
+    return render(request, "adminstaff.html")
